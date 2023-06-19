ff5174542a572f0b9fba927c57b279848e035f01
ncangigi <giorgio.cangioli@gmail.com>
2023-06-19 10:40:46 +0200

    copied maps from the HL7 EU guides

---
+++ input/fsh/maps/models/__syncLabRptMaps.bat
@@ -0,0 +1,20 @@
+
+copy C:\workspace\hl7eu-lab\input\fsh\models\*-map.fsh .
+
+for /R . %%f in (*.fsh) do (
+	echo %%f
+ CALL :processFshFile %%f  
+)
+
+
+:processFshFile
+
+ fart %~1  http://hl7.eu/fhir/laboratory/StructureDefinition/ http://hl7.eu/fhir/ig/xpandh/lab/StructureDefinition/
+ fart %~1  ServiceRequest-eu-lab ServiceRequest-lab-xpandh
+ fart %~1  Patient-eu-lab Patient-eu-xpandh
+ fart %~1  Bundle-eu-lab Bundle-lab-xpandh
+ fart %~1  http://hl7.eu/fhir/ig/xpandh/lab/StructureDefinition/information-recipient http://hl7.eu/fhir/StructureDefinition/information-recipient
+ 
+ GOTO :EOF
+
+
